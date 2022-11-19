de4404a07b3f8b9ec22c9d2b24161f33fe077e4b
Oscar <oscar@axovia.mx>
2022-11-19 16:08:59 -0600

    Initial setup
    -Instalation & configuration of prettier
    -Instalation & configuration of eslint
    -Instalation & configurations of husky

--- backend/.eslintrc.js
+++ backend/.eslintrc.js
@@ -2,7 +2,7 @@ module.exports = {
   parser: '@typescript-eslint/parser',
   parserOptions: {
     project: 'tsconfig.json',
-    tsconfigRootDir : __dirname, 
+    tsconfigRootDir: __dirname,
     sourceType: 'module',
   },
   plugins: ['@typescript-eslint/eslint-plugin'],
@@ -21,5 +21,21 @@ module.exports = {
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
+    eqeqeq: 'error',
+    'no-nested-ternary': 'error',
+    'no-var': 'error',
+    'no-console': 'warn',
+    'prefer-destructuring': 'warn',
+    'template-curly-spacing': 'warn',
+    'prettier/prettier': [
+      'warn',
+      {
+        tabWidth: 2,
+        semi: true,
+        bracketSpacing: true,
+        arrowParens: 'avoid',
+        endOfLine: 'auto',
+      },
+    ],
   },
 };
